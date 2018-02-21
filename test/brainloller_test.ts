c32402da9b44fb8008833265e013d8ff60ee88d8
Marcos Minond <minond.marcos@gmail.com>
2018-02-20 21:55:05 -0700

    fixing bl tests and adding one more test as well

--- test/brainloller_test.ts
+++ test/brainloller_test.ts
@@ -2,7 +2,8 @@ const assert = require('assert')
 const { readFileSync: read } = require('fs')
 const { exec: brainloller } = require('../dist/brainloller')
 
-const PROG_HELLO_WORLD = JSON.parse(read('./test/bl/helloworld.bl').toString())
+const PROG_HELLO_WORLD = JSON.parse(read('./test/bl/helloworld.json').toString())
+const PROG_FIB = JSON.parse(read('./test/bl/fibonacci.json').toString())
 
 const testsOutput = (label, prog, expectedOutput) => {
   var buff = []
@@ -20,3 +21,4 @@ const testsOutput = (label, prog, expectedOutput) => {
 }
 
 testsOutput('hello world', PROG_HELLO_WORLD, 'Hello World!')
+testsOutput('fibonacci', PROG_FIB, '1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 121, 98, 219, ...')
